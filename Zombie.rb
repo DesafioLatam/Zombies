530439fabcd3d20c4a08ff433727bf4cafa8ab2f
Jorge Vargas <jv.vargass@gmail.com>
2015-09-28 17:51:42 -0300

    The same of before with class Person

--- Zombie.rb
+++ Zombie.rb
@@ -1,25 +1,20 @@
-class Zombie
+class Zombie < Entity
   attr_accessor :name, :x, :y
-
-  def initialize(name = nil, posx = nil, posy = nil)
-    @name = name || 'Come cerebro'
-    @x = posx || rand(1..10)
-    @y = posy || rand(1..10)
+    
+  def initialize(name = nil, x = nil, y = nil)
+    super
+    @name = name || 'Come cerebros'
+    @type = 'Zombie'
   end
 
   def walk
-    @x += rand(-1..1) unless (@x >10 || @x < 1)
-    @y += rand(-1..1) unless (@y >10 || @y < 1)
+    @x += rand(-1..1) unless @x >10 || @x < 1
+    @y += rand(-1..1) unless @y >10 || @y < 1
   end
 
   def talk
     'BRAAAIIIINNN!!!'
   end
-
-  def to_s
-    puts "El zombie #{@name} esta en la posicion #{@x}, #{@y}"
-  end
-
 end
 
 pato = Zombie.new
